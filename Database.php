57278c5ced8e3812c511e9aa2b4f8d171c8cc317
Daniel Martinez <danielxz331@gmail.com>
2022-06-19 01:37:11 -0500

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,6 +1,6 @@
 <?php
 function conectardb(){
-	$conn_string = "host=ec2-52-72-56-59.compute-1.amazonaws.com port=5432 dbname=d9nsqgk9d088p8 user=jjoifiarzxgbxb password=8c9270753343850572afa47edace392672c2bf12acf7a8bf27ff64b5e1a6f303 ";
+	$conn_string = "host=ec2-52-72-56-59.compute-1.amazonaws.com port=5432 dbname=d9nsqgk9d088p8 user=jjoifiarzxgbxb password=8c9270753343850572afa47edace392672c2bf12acf7a8bf27ff64b5e1a6f303 options='--client_encoding=UTF8'";
 
 	// establecemos una conexion con el servidor postgresSQL
 	$dbconn = pg_connect($conn_string);
