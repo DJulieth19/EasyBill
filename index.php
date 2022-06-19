c5118acdb1d77814f48b86291fbbab11b501af49
Daniel Martinez <danielxz331@gmail.com>
2022-06-19 01:29:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,45 +8,14 @@
     
     <body> 
         <h1 align="center" >Mi Primera pagina web </h1> 
-        <hr> <p>pagina de los 3d wdf</p> 
-        <?php
-      	require_once "FuncionesCrud.php";
-				$users = get();
-				$nomb =$_GET['nomb'];
-        ?>
-        <?php
-        	if( !empty( $users ) ){
-        ?>
+        <hr> <p>pagina de los 3d wdf</p>         
 				<thead>
 					<tr>
 						<th><h1>ID USUARIO</h1></th>
 						<th><h1>NOMBRE USUARIO</h1></th>
 						<th><h1>TIPO USUARIO</h1></th>
 					</tr>
-				</thead>
-				<?php
-					$obj = json_decode($users, true); 
-					foreach( $obj as $user ){
-        ?>
-				<tbody>
-          <tr>
-            <td><?php echo $user['id_usuario'] ?></td>
-            <td><?php echo $user['nombre_usuario'] ?></td>
-            <td><?php echo $user['tipo_usuario'] ?></td>
-            <td> 
-          	</td> 
-        	</tr>
-				</tbody>
-      	<?php
-					}
-        ?>
-			</table>    
-      <?php
-      	}else{
-      ?>
-      <div class="alert alert-danger" style="margin-top: 100px">No hay ningun horario registrado</div>
-      <?php
-      	}
-      ?>
+				</thead>			
+				<tbody>  
     </body> 
 </html>
