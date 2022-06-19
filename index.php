37c9cd8418c533dfb6afe376bc0ab00a2120c0bf
Daniel Martinez <danielxz331@gmail.com>
2022-06-19 01:18:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,53 +6,8 @@
         <title>EasyBill </title> 
     </head>
     
-    <body>
-     <?php
-      	require_once "FuncionesCrud.php";
-				$users = get();
-				$nomb =$_GET['nomb'];
-      ?>
-			<table class="container">
-        <div class="conb">  
-        	<div class="spaceb1"></div>      	
-        	
-        	<div class="spaceb2"></div>
-        </div>
-        <?php
-        	if( !empty( $users ) ){
-        ?>
-				<thead>
-					<tr>
-						<th><h1>ID USUARIO</h1></th>
-						<th><h1>NOMBRE USUARIO</h1></th>
-						<th><h1>TIPO DE USUARIO</h1></th>
-					</tr>
-				</thead>
-				<?php
-					$obj = json_decode($users, true); 
-					foreach( $obj as $user ){
-        ?>
-				<tbody>
-          <tr>
-            <td><?php echo $user['id_usuario'] ?></td>
-            <td><?php echo $user['nombre_usuario'] ?></td>
-            <td><?php echo $user['tipo_usuario'] ?></td>
-            <td> 
-
-            	
-          	</td> 
-        	</tr>
-				</tbody>
-      	<?php
-					}
-        ?>
-			</table>    
-      <?php
-      	}else{
-      ?>
-      <div class="alert alert-danger" style="margin-top: 100px">No hay ningun horario registrado</div>
-      <?php
-      	}
-      ?>
+    <body> 
+        <h1 align="center" >Mi Primera pagina web </h1> 
+        <hr> <p>pagina de los 3d wdf</p> 
     </body> 
 </html>
