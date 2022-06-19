f3c3298604da025557e566bd5b7cfd2ca1017c4b
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-06-18 23:58:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head> 
+        <title>EasyBill </title> 
+    </head>
+    
+    <body> 
+        <h1 align="center" >Mi Primera pagina web </h1> 
+        <hr> <p>pagina de los 3d wdf</p> 
+ 
+    </body> 
+</html>
 
