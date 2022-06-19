cc5a1ae2c1d602eef4e1e94513cd85bb23741d4a
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-06-19 00:17:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <!DOCTYPE html>
-
 <html lang="en">
-
-    <head> 
-        <title>EasyBill </title> 
-    </head>
-    
-    <body> 
-        <h1 align="center" >Mi Primera pagina web </h1> 
-        <hr> <p>pagina de los 3d wdf</p> 
- 
-    </body> 
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/css/logi.css">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+	<link rel="shortcut icon" href="./img/icon.png" type="image/png">
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+ 	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	<title>EasyBill</title>
+</head>
+<body>
+	<header>
+	 	<div class="login-box">
+		    <img src="./assets/img/logol.png" class="avatar" alt="Avatar Image">
+		    <h1>Login</h1>
+		    <form action = "sesion.php" method = "POST">
+		      <label for="username">Usuario</label>
+		      <input type="text" name="user" placeholder="Ingrese el usuario">
+		      <label for="password">Contraseña</label>
+		      <input type="password" name="contra" placeholder="Ingrese la contrasela">
+		      <br><br>
+		      <input type="submit" value="Log In">
+		    </form>
+		  </div>		
+	</header>
+</body>
 </html>
-
