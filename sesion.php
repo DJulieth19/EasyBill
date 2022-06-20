87765074dd54282f27c8e278decaf11f11a07fed
Daniel Martinez <danielxz331@gmail.com>
2022-06-20 03:39:21 -0500

    Update sesion.php

--- sesion.php
+++ sesion.php
@@ -6,10 +6,10 @@ $conn = conectardb();
 $usuario = $_POST['user'];
 $contraseña = $_POST['contra'];
 
-$queryUsuarios = "SELECT * from usuarios WHERE nombre_usuario = '$usuario' AND contrasenia = '$contraseña'";
+$queryUsuarios = "SELECT tipo_usuario from usuarios WHERE nombre_usuario = '$usuario' AND contrasenia = '$contraseña'";
 $consultaUsuarios = pg_query($conn, $queryUsuarios);
 
-if($usuario == "Derly Varón"){
+if($queryUsuarios == "Administrador"){
 	header("location: ../Administrador/index.php");
 	exit();
 }else {
