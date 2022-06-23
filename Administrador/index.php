e215598a62767281ef9e0a93f630b8f8a86cb38d
David Acosta <dacostaojeda2000@gmail.com>
2022-06-22 19:03:38 -0500

    Update index.php

--- Administrador/index.php
+++ Administrador/index.php
@@ -8,7 +8,7 @@
     
     <body> 
         <h1 align="center" >Mi Primera pagina web </h1> 
-        <hr><p>pagina de los 3d</p></hr>
+        <hr><p>pagina de los 3d ricolino</p></hr>
         <?php
       	require_once "FuncionesCrud.php";
 				$users = get();
