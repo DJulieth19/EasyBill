d50e187718771f45c0e61ad27785ab878e252e86
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-07-02 23:50:53 -0500

    Create RestaurarContraseña.php

---
+++ Login/RestaurarContraseña.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./estilos.css">
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+	<link rel="shortcut icon" href="./img/icon.png" type="image/png">
+	<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+ 	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+	<title>EasyBill</title>
+</head>
+<body>
+	<header>
+	 	<div class="login-box">
+		    <img src="./logol.png" class="avatar" alt="Avatar Image">
+		    <h1>Recuperación de contraseña</h1>
+		    <form>
+		      <label for="username">Usuario</label>
+		      <input type="text" name="user">
+		      <label for="preguntas">Contraseña</label>
+		      <input type="text" name="texto" placeholder="Ingrese su respuesta">
+           <label for="preguntas">Contraseña</label>
+		      <input type="text" name="texto" placeholder="Ingrese su respuesta">
+           <label for="preguntas">Contraseña</label>
+		      <input type="text" name="texto" placeholder="Ingrese su respuesta">
+		      <br><br>
+		      <input type="submit" value="Validar">
+		    </form>
+		  </div>		
+	</header>
+</body>
+</html>
+
+
