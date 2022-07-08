8ab6989bbb7b528efee87baa6f81f0a4b62caf4a
Derly Varón <djvd2001@gmail.com>
2022-07-08 18:02:51 -0500

    cambio de imagenes

--- Empleado/index.php
+++ Empleado/index.php
@@ -130,7 +130,7 @@
       </div><!-- /.col-lg-4 -->
 
       <div class="col-lg-4">
-        <a href="https://example.com/" class="functions-img">
+        <a href="https://example.com/" class="functions">
           <img class="img" src="../Imagenes/historial1.png" href= class="bd-placeholder-img rounded-circle" width="180" height="160" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
           <img class="img" src="../Imagenes/historial2.png" href= class="bd-placeholder-img rounded-circle" width="180" height="160" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
         </a> 
