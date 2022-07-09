d94a83b79ecf33b0a837367f95d6baf8ed2269de
Derly Varón <djvd2001@gmail.com>
2022-07-09 00:07:14 -0500

    Update index.php

--- Empleado/index.php
+++ Empleado/index.php
@@ -129,15 +129,14 @@
         </a> 
         <h3 class="fw-normal">Registrar Pedido</h3>
         <!-- Modal -->
-        <div class="modal position-static d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modal">
-          <div class="modal-dialog" role="document">
+        <div class="modal bg-secondary py-5" tabindex="-1"  id="staticBackdrop">
+          <div class="modal-dialog">
             <div class="modal-content rounded-4 shadow">
               <div class="modal-header p-5 pb-4 border-bottom-0">
                 <!-- <h5 class="modal-title">Modal title</h5> -->
                 <h2 class="fw-bold mb-0">Datos del cliente</h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-
               <div class="modal-body p-5 pt-0">
                 <form class="">
                   <div class="form-floating mb-3">
@@ -149,6 +148,7 @@
                     <label for="id">Identificacion</label>
                   </div>
                   <button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Continuar</button>
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 </form>
               </div>
             </div>
