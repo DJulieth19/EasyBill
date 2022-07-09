8cc98f24cae6785a121634068e7ac7407ce6f785
Derly Varón <djvd2001@gmail.com>
2022-07-08 22:39:25 -0500

    Update index.php

--- Empleado/index.php
+++ Empleado/index.php
@@ -126,12 +126,14 @@
       <div class="col-lg-4">
         <a data-toggle="modal" data-target="#exampleModalCenter">
           <img src="../../Imagenes/RegistrarPedido.png"  href= class="bd-placeholder-img rounded-circle" width="160" height="160" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
-        <!-- Button trigger modal -->
-        <button type="button" color="white" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-          Registrar Pedido
-        </button>        
+        
         </a> 
 
+      </div><!-- /.col-lg-4 -->
+        <!-- Button trigger modal -->
+              <button type="button" color="white" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          Registrar Pedido
+        </button>
         <!-- Modal -->
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
@@ -150,8 +152,6 @@
             </div>
           </div>
         </div>
-      </div><!-- /.col-lg-4 -->
-
       <div class="col-lg-4">
         <a href="https://example.com/" class="functions">
           <img class="img" src="../Imagenes/historial1.png" href= class="bd-placeholder-img rounded-circle" width="180" height="160" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
