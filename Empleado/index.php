5e1206e0777e996f589d83977b341901f6ad4d84
Derly Varón <djvd2001@gmail.com>
2022-07-08 23:25:54 -0500

    Update index.php

--- Empleado/index.php
+++ Empleado/index.php
@@ -132,11 +132,11 @@
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title text-center bg-success text-white" id="staticBackdropLabel">Datos del cliente</h5>
+              <div class="modal-header  bg-primary text-center ">
+                <h5 class="modal-title" id="staticBackdropLabel">Datos del cliente</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-                <div class="modal-body">
+                <div class="modal-body text-left">
                   <div class="d-flex flex-column">                 
                     <label for="nombreCliente">Nombre del cliente</label>
                     <input type="text" name="nombreCliente">
