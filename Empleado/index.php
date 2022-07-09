74a82702b66e424611ba4cbb798c81548bf0ca1c
Derly Varón <djvd2001@gmail.com>
2022-07-08 23:23:35 -0500

    Update index.php

--- Empleado/index.php
+++ Empleado/index.php
@@ -133,7 +133,7 @@
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title text-center" background="blue" id="staticBackdropLabel">Datos del cliente</h5>
+                <h5 class="modal-title text-center bg-success text-white" id="staticBackdropLabel">Datos del cliente</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
                 <div class="modal-body">
