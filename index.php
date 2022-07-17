4e01493035120204675e197aae9951adb3dc28fd
David Acosta <dacostaojeda2000@gmail.com>
2022-07-16 19:40:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <title>EasyBill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <style>
+	body{
+		background: #ffee23;
+		background: linear-gradient(to right, #9F70DC, #6211F0);
 	.bg{
 		background-image: url(./img/fondo.png);
 		background-position: center center;
