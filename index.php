ec80d609686a6478d3822cb2efd16fd08f2c2714
David Acosta <dacostaojeda2000@gmail.com>
2022-07-16 19:29:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>EasyBill</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-  </head>
+<style>
+	.bg{
+		background-image: url(imagenes/fondo.png);
+		background-position: center center;
+	}
+</style>
+</head>
   <body>
     <div class="container">
 		<div class="row">
-			<div class="col">
+			<div class="col bg">
 
 			</div>
 			<div class="col">
