8dde71fc116310ea98433d5712f9c120618a9419
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-07-16 22:30:57 -0500

    Update Actividad.php

--- Administrador/ConsultaActividad/Actividad.php
+++ Administrador/ConsultaActividad/Actividad.php
@@ -56,7 +56,7 @@
   </header>
   <main>
     <body>
-      <table class="table-responsive col-md-3 offset-md-3">
+      <table class="table-responsive col-md-6 offset-md-3">
         <thead>
           <tr>
             <th scope="col">#</th>
