888b6862ccabc4eebb8727bb4ee7785d6ab0e5e8
David Acosta <dacostaojeda2000@gmail.com>
2022-07-16 20:26:38 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@
 </style>
 </head>
   <body>
-    <div class="container w-75 bg-light pt-5 rounded shadow">
+    <div class="container w-75 bg-light mt-5 rounded-pill shadow">
 		<div class="row aling-items-stretch">
-			<div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6">
+			<div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded-pill">
 			</div>
 			<div class="col">
 				<div class="text-end">
