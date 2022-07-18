25d22b1603e9845dfa9f0280c730625749525aeb
Derly Varón <djvd2001@gmail.com>
2022-07-18 16:42:06 -0500

    Update Actividad.php

--- Administrador/ConsultaActividad/Actividad.php
+++ Administrador/ConsultaActividad/Actividad.php
@@ -26,7 +26,8 @@
           ?>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
           <div class="container-fluid">
-              <a class="navbar-brand">Consulta de Actividad</a>
+              
+              <h3>Consulta de Actividad</h3>
               <button class="navbar-toggler "  type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
