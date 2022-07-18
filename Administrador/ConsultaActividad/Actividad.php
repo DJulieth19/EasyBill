5f9d8454459d3b858966a2ccfcbc60341c13d860
Daniel Martinez <danielxz331@gmail.com>
2022-07-18 10:45:13 -0500

    Update Actividad.php

--- Administrador/ConsultaActividad/Actividad.php
+++ Administrador/ConsultaActividad/Actividad.php
@@ -8,10 +8,13 @@
     <meta name="generator" content="Hugo 0.98.0">
     <title>Administrador</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
-
-<link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
     <!-- Custom styles for this template -->
     <link href="../styles.css" rel="stylesheet">
   </head>
@@ -58,7 +61,7 @@
   <main>
     <body>
       <h1> &nbsp </h1>
-      <table class="table-hover col-md-6 offset-md-3 t-5">
+      <table class="table-responsive table-hover col-md-6 offset-md-3 t-5">
         <thead class="table-info">
           <tr>
             <th scope="col">#</th>
