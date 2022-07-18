597e1fd26d507f8e74acafe1485bb4f7ccc24c5c
Daniel Martinez <danielxz331@gmail.com>
2022-07-18 11:25:11 -0500

    Update Actividad.php

--- Administrador/ConsultaActividad/Actividad.php
+++ Administrador/ConsultaActividad/Actividad.php
@@ -26,7 +26,7 @@
           ?>
           <nav class="navbar navbar-expand-md fixed-top bg-dark">
             <div class="container-fluid">
-                <h3>&nbsp &nbsp</h3>
+                <h3>something</h3>
                 <h3>Consulta de Actividad</h3>
                 <button class="navbar-toggler "  type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
