e424d08c82a3d8a15c799563aa3873999ce19f90
Derly Varón <djvd2001@gmail.com>
2022-07-18 21:25:21 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -64,16 +64,16 @@
   <main>
     <body>
       <h1> &nbsp </h1>
-      <div class="container-sm">
-        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-          <label class="btn btn-outline-primary" for="btnradio1">Hoy</label>
+      <div class="container-sm mx-auto">
+        <div class="btn-group b-1" role="group"  aria-label="Basic radio toggle button group">
+          <input type="radio" class="btn-check" name="btnradio" autocomplete="off" checked>
+          <label class="btn btn-outline-primary" >Hoy</label>
 
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-          <label class="btn btn-outline-primary" for="btnradio2">Semana</label>
+          <input type="radio" class="btn-check" name="btnradio" autocomplete="off">
+          <label class="btn btn-outline-primary" >Semana</label>
 
-          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-          <label class="btn btn-outline-primary" for="btnradio3">Mes</label>
+          <input type="radio" class="btn-check" name="btnradio" autocomplete="off">
+          <label class="btn btn-outline-primary" >Mes</label>
         </div>
       </div>
 
