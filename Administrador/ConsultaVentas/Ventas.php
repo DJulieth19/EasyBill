b0ad9eb14c8bfd173e911c4a7b5322de905eb21c
Derly Varón <djvd2001@gmail.com>
2022-07-19 17:55:44 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -29,7 +29,7 @@
       <div class="col-md-4 space-blank">
         <h3>&nbsp &nbsp</h3>
       </div>
-        <h2>Consulta de Ventas</h2>
+        <h2>Consulta de Ventasssss</h2>
         <button class="navbar-toggler "  type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
