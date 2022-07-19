f52c2e919007c324542cac8bc9c03b5078c226bb
Derly Varón <djvd2001@gmail.com>
2022-07-18 21:03:46 -0500

    Botones de ventas

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -67,7 +67,7 @@
 
       <div class="container-botones">
         <div class="center">
-         <button class="btn-dark">
+         <button class="btn-days">
             <svg width="90px" height="30px" viewBox="0 0 180 60" class="border">
               <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
               <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
