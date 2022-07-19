91e7fef348ae9ed8f4a0f7b59885aeba7e0ba5bd
Derly Varón <djvd2001@gmail.com>
2022-07-19 18:13:32 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -60,33 +60,59 @@
   </nav>
 </header>
 
-  <main>
+<main>
     <body>
-      <h1> &nbsp </h1>
-      <div class="container">
+    <h1 class="text-white col-md-3 space">space </h1> 
+    <h4 class="text-white col-md-3 space">space </h4> 
+      <div class="container mt-8">
+        <div class="row justify-content-md-center">
+		<div class="btn-group" role="group">
+		  <h4 class="text-white col-md-3 space">space </h4> 
+		  <button type="button" class="btn btn-outline-primary">Hoy</button>
+		  <button type="button" class="btn btn-outline-primary">Semana</button>
+		  <button type="button" class="btn btn-outline-primary">Mes</button>
+		  <h4 class="text-white col-md-3">space </h4> 
+		  <a>
+		      <svg xmlns="http://www.w3.org/2000/svg" color="blue" width="25" height="25" fill="currentColor" class="bi bi-calendar-date" viewBox="0 0 16 16">
+			<path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z"></path>
+			<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"></path>
+		      </svg>
+		  </a>
+		</div>
+        </div>
+      </div>
+
+      <div class="container mt-4">
         <div class="row justify-content-md-center">
           <div class="table-responsive table-striped table-bordered">
             <table class="table table-hover">
               <thead class ="table-primary">
                   <tr>
-                    <th class="col-4 text-center">Usuario</th>
-                    <th class="col-2 text-center">Campo</th>
-                    <th class="col-3 text-center">Comando</th>
-                    <th class="col-4 text-center">Fecha y hora</th>
+                    <th class="col-4 text-center">Producto</th>
+                    <th class="col-1 text-center">Cantidad</th>
+                    <th class="col-3 text-center">Total</th>
                   </tr>
               </thead>
               <tbody>
                     <tr>
-                        <td class="col-4" scope="row">user</td>
-                        <td class="col-2">Nissan</td>
-                        <td class="col-3">Qasqai</td>
-                        <td class="col-4">2017</td>
+                        <td class="col-4" scope="row">Arroz con pollo</td>
+                        <td class="col-2 text-center">2</td>
+                        <td class="col-3 text-center">24.000</td>
                     </tr>
               </tbody>
             </table>
           </div>
       </div>
 
+ 	<div class="container">
+	    <div class="row justify-content-md-center">
+	      <form class="d-flex" role="search">
+		<h5> Total de ventas &nbsp </h5> 
+		<span class="border">Total$$$$$</span>
+              </form>
+            </div>
+	</div>
+	      
       <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
     </body>
   </main>
