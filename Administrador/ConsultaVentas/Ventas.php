56c76ad5fce3ab211cb765fa58314d365ced572b
Derly Varón <djvd2001@gmail.com>
2022-07-18 21:17:32 -0500

    Botones

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -17,7 +17,7 @@
 
     <!-- Custom styles for this template -->
     <link href="../styles.css" rel="stylesheet">
-    <link href="stylesButton.css" rel="stylesheet">
+
   </head>
   <header>
 <?php
@@ -64,19 +64,17 @@
   <main>
     <body>
       <h1> &nbsp </h1>
+      <div class="container-sm">
+        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+          <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+          <label class="btn btn-outline-primary" for="btnradio1">Hoy</label>
 
-      <div class="container-botones">
-        <div class="center">
-          <button class="btn-botones">
-            <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
-              <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
-              <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
-            </svg>
-            <span>HOVER ME</span>
-          </button>
+          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+          <label class="btn btn-outline-primary" for="btnradio2">Semana</label>
+
+          <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+          <label class="btn btn-outline-primary" for="btnradio3">Mes</label>
         </div>
-      </div>
-
       </div>
 
       <div class="container">
