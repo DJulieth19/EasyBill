64b58fb1daa21e6c32d33f1c45722edd98750208
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-07-19 10:40:02 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -36,7 +36,7 @@
         <div class="ml-auto">
             <div class="collapse navbar-collapse" id="navbarCollapse">
               <div class="nav-bar">
-              <form class="d-flex" role="search">
+              <form class="d-flex" >
                 <div class="NombreUser">
                   <thead>
                       <th>
@@ -66,11 +66,12 @@
       <h1> &nbsp </h1>
       <div class="container">
         <div class="row justify-content-md-center">
-		<div class="btn-group" role="group" aria-label="Basic example">
+		<div class="btn-group" role="group">
+		  <h4 class="text-white col-md-4">space </h4> 
 		  <button type="button" class="btn btn-outline-primary">Hoy</button>
 		  <button type="button" class="btn btn-outline-primary">Semana</button>
 		  <button type="button" class="btn btn-outline-primary">Mes</button>
-		  <h4 class="text-white col-md-6">space </h4> 
+		  <h4 class="text-white col-md-4">space </h4> 
 		  <a>
 		      <svg xmlns="http://www.w3.org/2000/svg" color="blue" width="25" height="25" fill="currentColor" class="bi bi-calendar-date" viewBox="0 0 16 16">
 			<path d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z"></path>
@@ -105,11 +106,13 @@
 
  	<div class="container mt-6">
 	    <div class="row justify-content-md-center">
-		<h4> Total de ventas </h4> 
+	      <form class="d-flex" role="search">
+		<h6> Total de ventas </h6> 
 		<label> total </label>
+              </form>
             </div>
 	</div>
-
+	      
       <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
     </body>
   </main>
