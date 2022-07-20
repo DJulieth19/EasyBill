41950cef2e1f7a7d1bdf8f77ae1bc1c524463bf6
Derly Varón <djvd2001@gmail.com>
2022-07-19 19:19:43 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -72,24 +72,43 @@
     </nav>
 </header>
 <style>
-    body {
-        margin-top: 5%;
-    }
+body {
+    margin-top: 5%;
+}
+
+.container-1 {
+    display: initial;
+}
+
 @media screen and (max-width:400px) {
     body {
         margin-top: 25%;
     }
+
+    .container-2 {
+        display: initial;
+    }
 }
+
 @media screen and (min-width:400px) and (max-width:999px) {
     body {
         margin-top: 8%;
+    }
+
+    .container-2 {
+        display: none;
+    }
+
+    .container-1 {
+        display: initial;
     }
 }
 </style>
 
 <main>
+
     <body>
-    <div class="container container-1">
+        <div class="container container-1">
             <div class="row justify-content-md-center">
                 <div class="btn-group" role="group">
                     <h4 class="text-white col-md-3 space">space </h4>
@@ -120,7 +139,7 @@
                     <button type="button" class="btn btn-outline-primary">Mes</button>
                     <a>
                         <svg xmlns="http://www.w3.org/2000/svg" color="blue" width="25" height="25" fill="currentColor"
-                            class="bi bi-calendar-date" viewBox="0 0 16 16">
+                            class="bi bi-calendar-date ml-2" viewBox="0 0 16 16">
                             <path
                                 d="M6.445 11.688V6.354h-.633A12.6 12.6 0 0 0 4.5 7.16v.695c.375-.257.969-.62 1.258-.777h.012v4.61h.675zm1.188-1.305c.047.64.594 1.406 1.703 1.406 1.258 0 2-1.066 2-2.871 0-1.934-.781-2.668-1.953-2.668-.926 0-1.797.672-1.797 1.809 0 1.16.824 1.77 1.676 1.77.746 0 1.23-.376 1.383-.79h.027c-.004 1.316-.461 2.164-1.305 2.164-.664 0-1.008-.45-1.05-.82h-.684zm2.953-2.317c0 .696-.559 1.18-1.184 1.18-.601 0-1.144-.383-1.144-1.2 0-.823.582-1.21 1.168-1.21.633 0 1.16.398 1.16 1.23z">
                             </path>
