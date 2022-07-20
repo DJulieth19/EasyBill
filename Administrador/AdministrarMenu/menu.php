d5208ab1eece37681dcfd60ae22cf5502d976b44
Derly Varón <djvd2001@gmail.com>
2022-07-20 18:26:41 -0500

    Update menu.php

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -4,9 +4,7 @@ $conn = conectardb();
 $queryPlatos = "SELECT * from productos";
 $consultaPlatos = pg_query($conn, $queryPlatos);
 
-
 $nombreProducto=$_GET['nombreproducto'];
-$platos= pg_fetch_array($consultaPlatos);
 $queryPlato = "SELECT * from producto WHERE nombreproducto = '$nombreProducto'";
 $consultaPlato = pg_query($conn, $queryPlato);
 $plato= pg_fetch_array($consultaPlato);
