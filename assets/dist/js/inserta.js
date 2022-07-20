4cf57de423ef754a6b00bf7d034634055f57860c
Derly Varón <djvd2001@gmail.com>
2022-07-20 14:23:01 -0500

    Update inserta.js

--- assets/dist/js/inserta.js
+++ assets/dist/js/inserta.js
@@ -1,9 +1,22 @@
- 
+function btnSaveLoad() {
+    $("#btn_save").html('Guardando ...');
+    $("#btn_save").attr("disabled", true);
+}
+
+function btnSave() {
+    $("#btn_save").html('Guardar');
+    $("#btn_save").attr("disabled", false);
+}
+
 $(document).ready(function() {
 
     $("#frm_foto").unbind('submit').bind('submit', function(){
+
+        var nombreProducto = $('#nombreProducto').val();
+        var valor = $('#valor').val();
+
             $.ajax({
-                url: 'save_img.php',
+                url: 'backend/save_img.php',
                 type: 'POST',
                 data: new FormData(this),
                 cache: false,
@@ -12,7 +25,18 @@ $(document).ready(function() {
                 beforeSend: function(){
                     btnSaveLoad();
                 },
-            });  
+                success: function(response){
+                    btnSave();
+                    if (response.success == true) {
+                        swal("MENSAJE", response.messages , "success");
+                        $("#frm_foto")[0].reset();
+                        $("#radiosfoto").click();
+                    } else {
+                        swal("MENSAJE", response.messages , "error");
+                    }
+                }
+            });
     });
+
 
 });
