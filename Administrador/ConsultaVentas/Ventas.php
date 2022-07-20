c91e8df79dcaae49d5fd3caacf00bca9cf523c13
Derly Varón <djvd2001@gmail.com>
2022-07-19 19:44:08 -0500

    Update Ventas.php

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -116,7 +116,7 @@ body {
 
     <body>
         <div class="container container-1">
-            <div class="row justify-content-md-center col-md-8">
+            <div class="row justify-content-md-center col-md-10">
                 <div class="btn-group" role="group">
                     <h4 class="text-white col-md-3 space">space </h4>
                     <button type="button" class="btn btn-outline-primary">Hoy</button>
