dba4c793f9a712791bab7c5a2f6237e6a949bd8d
Derly Varón <djvd2001@gmail.com>
2022-07-20 17:01:47 -0500

    Update añadir.php

--- Administrador/AdministrarMenu/añadir.php
+++ Administrador/AdministrarMenu/añadir.php
@@ -5,7 +5,7 @@ $nombreProducto =$_POST['nombreProducto'];
 $Precio=$_POST['Precio'];
 $imagen=false;
 
-$queryPlatos = "INSERT INTO productos (nombreproducto,precio,imagen)  values ('"$nombreProducto"','"$Precio"','"$imagen"');";
+$queryPlatos = "INSERT INTO productos (nombreproducto,precio,imagen)  values ('$nombreProducto','$Precio','$imagen');";
 $insertarPlatos = pg_query($conn, $queryPlatos);
 
 header("location: ./menu.php");
