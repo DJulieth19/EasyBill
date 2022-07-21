af77dc0380d0f8e3eb80be497411de0aa10cea10
Derly Varón <djvd2001@gmail.com>
2022-07-20 20:44:39 -0500

    Update editar.php

--- Administrador/AdministrarMenu/editar.php
+++ Administrador/AdministrarMenu/editar.php
@@ -13,12 +13,12 @@ $consultaUsuarios = pg_query($conn, $queryUsuarios);
 $usuario1= pg_fetch_array($consultaUsuarios);
 $codproducto=$usuario1['codproducto'];
 
-$queryPlatos = "UPDATE productos SET  nombreproducto='$nombreproducto',precio='$precio' when codproducto='$codproducto'";
+$queryPlatos = "UPDATE productos SET  nombreproducto='$nombreproducto',precio='$precio' WHERE codproducto='$codproducto'";
 $editarPlatos = pg_query($conn, $queryPlatos);
 
 echo $nombreproducto;
 echo $precio;
 
-##header("location:./menu.php?nombre=$usuario&tipoUsuario=$tipo");
-##exit();
+header("location:./menu.php?nombre=$usuario&tipoUsuario=$tipo");
+exit();
 ?>
