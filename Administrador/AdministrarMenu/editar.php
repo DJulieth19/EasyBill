e5b06a224814bacb370a16cc5881b60918fb660d
Derly Varón <djvd2001@gmail.com>
2022-07-20 19:22:06 -0500

    editar platos

--- Administrador/AdministrarMenu/editar.php
+++ Administrador/AdministrarMenu/editar.php
@@ -1,15 +1,21 @@
+<?php
+    $usuario = $_GET['nombre'];
+    $tipo = $_GET['tipoUsuario'];
+?>
 <?php 
 require_once("../../Database.php");
 $conn = conectardb();
-$nombreProducto=$_GET['nombreproducto'];
-$nombreProducto=$_POST['nombreproducto'];
-$Precio=$_POST['precio'];
-
-$queryPlato = "UPDATE productos SET nombreproducto = '$nombreProducto',precio = '$Precio'";
-$consultaPlato = pg_query($conn, $queryPlato);
-$plato= pg_fetch_array($consultaPlato);
-$Precio=$plato['precio'];
-
-header("location: ./usuarios.php");
+$id_usuario=$_GET['id_usuario'];
+$nombre_usuario=$_POST['nombre_usuario'];
+$contraseña=$_POST['contraseña'];
+$tipo_usuario=$_POST['tipo_usuario'];
+$cedula=$_POST['cedula'];
+$fexpedicion = $_POST['fexpedicion'];
+$celular = $_POST['celular'];
+$queryUsuarios = "UPDATE usuarios SET nombre_usuario='$nombre_usuario',contrasenia='$contraseña',tipo_usuario='$tipo_usuario' WHERE id_usuario='$id_usuario'";
+$editarUsuarios = pg_query($conn, $queryUsuarios);
+$queryRecuperacion = "UPDATE recuperacion SET cedula='$cedula',fexpedicion='$fexpedicion',celular='$celular' WHERE id_usuario='$id_usuario'";
+$editarRecuperacion= pg_query($conn, $queryRecuperacion);
+header("location:./usuarios.php?nombre=$usuario&tipoUsuario=$tipo");
 exit();
 ?>
