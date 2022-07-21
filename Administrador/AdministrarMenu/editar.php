786640f9507bde37ad56f26bc0450d411ae50bca
Derly Varón <djvd2001@gmail.com>
2022-07-20 20:25:43 -0500

    Update editar.php

--- Administrador/AdministrarMenu/editar.php
+++ Administrador/AdministrarMenu/editar.php
@@ -9,9 +9,8 @@ $nombreproducto=$_GET['nombreproducto'];
 $precio=$_GET['precio'];
 
 $queryPlatos = "UPDATE productos SET nombreproducto='$nombreproducto',precio='$precio'";
-
 $editarPlatos = pg_query($conn, $queryPlatos);
 
-header("location:./menu.php?nombre=$usuario&tipoUsuario=$tipo");
-exit();
+##header("location:./menu.php?nombre=$usuario&tipoUsuario=$tipo");
+##exit();
 ?>
