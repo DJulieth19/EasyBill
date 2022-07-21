a8798923ec4b7ef527190759c448f2966724af30
Derly Varón <djvd2001@gmail.com>
2022-07-21 18:43:22 -0500

    Update borrar.php

--- Administrador/AdministrarMenu/borrar.php
+++ Administrador/AdministrarMenu/borrar.php
@@ -9,6 +9,10 @@ $precio=$_POST['precio'];
 $queryProductos = "DELETE FROM productos WHERE codproducto = '$codproducto'";
 $borrarProductos = pg_query($conn, $queryProductos);
 
+echo $codproducto;
+echo $nombreproducto;
+echo $precio;
+
 ##header("location: ./menu.php");
 ##exit();
 ?>
