d065062a3d61bec69b5a3f160d41faf9c9b01baf
Derly Varón <djvd2001@gmail.com>
2022-07-21 19:51:16 -0500

    Añadiendo icon a admin y empleado

--- Administrador/ConsultaVentas/Ventas.php
+++ Administrador/ConsultaVentas/Ventas.php
@@ -4,9 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.98.0">
+    <link rel="icon" href="../img/icon.png">
     <title>ConsultarActividad</title>
 
     <link href="../../assets/dist/css/bootstrap.min.css" rel="stylesheet">
