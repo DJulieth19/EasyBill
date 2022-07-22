93ffbb85f8d8a76c4873ca91868e8a8655d07685
Derly Varón <djvd2001@gmail.com>
2022-07-21 19:53:44 -0500

    Update index.php

--- Administrador/index.php
+++ Administrador/index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="../img/icon.png">
     <title>Administrador</title>
+    <link rel="icon" href="../../img/icon.png">
     <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
     <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
