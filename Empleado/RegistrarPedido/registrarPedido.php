9c9d90bf962b83117dc26ee1a5f76f91dc059df5
Derly Varón <djvd2001@gmail.com>
2022-07-30 23:23:03 -0500

    Update registrarPedido.php

--- Empleado/RegistrarPedido/registrarPedido.php
+++ Empleado/RegistrarPedido/registrarPedido.php
@@ -70,7 +70,6 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
         <h1 class="text-white"> espacio</h1>
         <div class="row">
             <div class="col col-md-6 ml-4">
-
                 <!-- START SECTION STORE -->
                 <section class="store ">
                     <div class="container">
@@ -82,9 +81,9 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                                         ?>
                                 <div class="col-lg-4">
                                     <div class="item shadow mb-4">
-                                        <h3 class="item-title">Plato 1</h3>
+                                        <h3 class="item-title"><?php echo $cantidadProductos['nombreproducto'] ?></h3>
                                         <img class="item-image " src="./img/plato.png">
-                                        <h4 class="item-price text-center">$ 10.000</h4>
+                                        <h4 class="item-price text-center">$ <?php echo $cantidadProductos['precio'] ?></h4>
                                         <div class="display-flex">
                                             <button
                                                 class="item-button btn-lg btn-info align-items-center addToCart">Añadir
@@ -98,79 +97,79 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                             </div>
                         </div>
                     </div>
+                </section>
             </div>
-            </section>
             <!-- END SECTION STORE -->
-        </div>
-        <div class="col col-md-5">
-            <!-- START SECTION SHOPPING CART -->
-            <section class="shopping-cart shadow">
-                <div class="container">
-                    <h1 class="text-center">
-                        Carrito
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                            class="bi bi-cart-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                        </svg>
-                    </h1>
+            <div class="col col-md-5">
+                <!-- START SECTION SHOPPING CART -->
+                <section class="shopping-cart shadow">
+                    <div class="container">
+                        <h1 class="text-center">
+                            Carrito
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                class="bi bi-cart-fill" viewBox="0 0 16 16">
+                                <path
+                                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                            </svg>
+                        </h1>
 
-                    <hr>
-                    <div class="row">
-                        <div class="col-6">
-                            <div class="shopping-cart-header">
-                                <h6>Producto</h6>
-                            </div>
-                        </div>
-                        <div class="col-2">
-                            <div class="shopping-cart-header">
-                                <h6 class="text-truncate">Precio</h6>
-                            </div>
-                        </div>
-                        <div class="col-4">
-                            <div class="shopping-cart-header">
-                                <h6>Cantidad</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- ? START SHOPPING CART ITEMS -->
-                    <div class="shopping-cart-items shoppingCartItemsContainer">
-                    </div>
-                    <!-- ? END SHOPPING CART ITEMS -->
-
-                    <!-- START TOTAL -->
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="shopping-cart-total d-flex align-items-center">
-                                <p class="mb-0">Total</p>
-                                <p class="ml-4 mb-0 shoppingCartTotal">$0</p>
-                                <div class="toast ml-auto bg-info" role="alert" aria-live="assertive" aria-atomic="true"
-                                    data-delay="2000">
-                                    <div class="toast-header">
-                                        <span>✅</span>
-                                        <strong class="mr-auto ml-1 text-secondary">Elemento en el carrito</strong>
-                                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast"
-                                            aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="toast-body text-white">
-                                        Se aumentó correctamente la cantidad
-                                    </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="shopping-cart-header">
+                                    <h6>Producto</h6>
                                 </div>
-                                <button class="btn btn-success ml-auto comprarButton" type="button">Finalizar
-                                    compra</button>
+                            </div>
+                            <div class="col-2">
+                                <div class="shopping-cart-header">
+                                    <h6 class="text-truncate">Precio</h6>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="shopping-cart-header">
+                                    <h6>Cantidad</h6>
+                                </div>
                             </div>
                         </div>
+                        <!-- ? START SHOPPING CART ITEMS -->
+                        <div class="shopping-cart-items shoppingCartItemsContainer">
+                        </div>
+                        <!-- ? END SHOPPING CART ITEMS -->
+
+                        <!-- START TOTAL -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="shopping-cart-total d-flex align-items-center">
+                                    <p class="mb-0">Total</p>
+                                    <p class="ml-4 mb-0 shoppingCartTotal">$0</p>
+                                    <div class="toast ml-auto bg-info" role="alert" aria-live="assertive"
+                                        aria-atomic="true" data-delay="2000">
+                                        <div class="toast-header">
+                                            <span>✅</span>
+                                            <strong class="mr-auto ml-1 text-secondary">Elemento en el carrito</strong>
+                                            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="toast-body text-white">
+                                            Se aumentó correctamente la cantidad
+                                        </div>
+                                    </div>
+                                    <button class="btn btn-success ml-auto comprarButton" type="button">Finalizar
+                                        compra</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- END TOTAL -->
+
+
                     </div>
 
-                    <!-- END TOTAL -->
-
-
-                </div>
-
-            </section>
-            <!-- END SECTION SHOPPING CART -->
+                </section>
+                <!-- END SECTION SHOPPING CART -->
+            </div>
         </div>
     </div>
 
