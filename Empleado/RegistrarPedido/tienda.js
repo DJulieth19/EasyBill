e56494aadae489c11e5b95b022a9ace2efa47b4f
Derly Varón <djvd2001@gmail.com>
2022-07-31 00:32:08 -0500

    Update tienda.js

--- Empleado/RegistrarPedido/tienda.js
+++ Empleado/RegistrarPedido/tienda.js
@@ -97,7 +97,7 @@ function updateShoppingCartTotal() {
     );
     total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
   });
-  shoppingCartTotal.innerHTML = `${total.toFixed(0)}$`;
+  shoppingCartTotal.innerHTML = `$${total.toFixed(0)}`;
 }
 
 function removeShoppingCartItem(event) {
