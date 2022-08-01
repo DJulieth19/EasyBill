c883cbe0f6a6b683c515faf590b55856fe98f512
Derly Varón <djvd2001@gmail.com>
2022-08-01 18:30:11 -0500

    Config space

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -126,7 +126,7 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
 
                 </div>
                 <div class="col col-3">
-                    <div class="row justify-content-end">
+                    <div class="row">
                         <div class="col-4 functions" data-bs-toggle="modal" data-bs-target="#añadir">
                             <button type="button" class="btn btn-info rounded-circle">
                                 <img src="./img/plato2.png">
