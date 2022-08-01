654631c1f86f959cd436d6dfa8bb8ad03899d7dd
Daniel Martinez <danielxz331@gmail.com>
2022-08-01 18:40:33 -0500

    Update añadir.php

--- Administrador/AdministrarMenu/añadir.php
+++ Administrador/AdministrarMenu/añadir.php
@@ -3,7 +3,7 @@ require_once("../../Database.php");
 $conn = conectardb();
 $nombreProducto =$_POST['nombreProducto'];
 $Precio=$_POST['Precio'];
-$queryPlatos = "INSERT INTO productos (nombreproducto,precio,imagen)  values ('$nombreProducto','$Precio','false');";
+$queryPlatos = "INSERT INTO productos (nombreproducto,precio,ruta_imagen)  values ('$nombreProducto','$Precio','nohayfoto');";
 $insertarPlatos = pg_query($conn, $queryPlatos);
 
 header("location: ./menu.php");
