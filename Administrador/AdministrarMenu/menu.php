421fcee280cf13c87d00c839b64f0b30a534d43d
Derly Varón <djvd2001@gmail.com>
2022-08-01 16:30:47 -0500

    Update menu.php

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -74,54 +74,57 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
 
         <body>
             <h1> &nbsp </h1>
-            <div class="container">
-                <!-- Contenedor de platos -->
-                <div class="row justify-content-center">
-                    <?php
-                        while($cantidadProductos=pg_fetch_array($consultaPlatos)){
-                        ?>
-                    <div class="col-lg-4 border">
-                        <h6 class="fw-normal d-flex justify-content-md-center">
-                            <?php echo $cantidadProductos['nombreproducto'] ?></h6>
-                        <a
-                            href="../Administrador/AdministrarMenu/menu.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>">
-                            <img src="./img/plato.png" href=class="bd-placeholder-img" width="155" height="160"
-                                role="img" preserveAspectRatio="xMidYMid slice" focusable="false">
-                            <title>Placeholder</title>
-                            <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777"
-                                dy=".3em"></text></svg>
-                        </a>
-                        <div class="d-flex justify-content-md-center">
-                            <h7 class="fw-normal"> <?php echo $cantidadProductos['precio'] ?></h7>
-                        </div>
-                        <div class="d-flex justify-content-md-center">
-                            <button type="button" class="btn btn-warning btn-sm mr-2" name="editar"
-                                onclick="location.href='editarPlato.php?nombreproducto=<?php echo $cantidadProductos['nombreproducto'] ?>&precio=<?php echo $cantidadProductos['precio'] ?>&nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>'">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                    <path
-                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path fill-rule="evenodd"
-                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                </svg>
-                            </button>
-                            <h4>&nbsp</h4>
-                            <button type="button" class="btn btn-danger btn-sm" name="borrar"
-                                onclick="location.href='borrarPlato.php?nombreproducto=<?php echo $cantidadProductos['nombreproducto'] ?>&precio=<?php echo $cantidadProductos['precio'] ?>&nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>'">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-trash3" viewBox="0 0 16 16">
-                                    <path
-                                        d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                    </div><!-- /.col-lg-4 -->
-                    <?php
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col col-md-6 ml-4">
+                        <!-- Empieza contenedor de platos -->
+                        <section class="store ">
+                            <div class="container">
+                                <div class="items">
+                                    <!-- Contenedor de platos -->
+                                    <div class="row justify-content-center">
+                                        <?php
+                                        while($cantidadProductos=pg_fetch_array($consultaPlatos)){
+                                        ?>
+                                        <div class="col-lg-4">
+                                            <div class="item shadow mb-4">
+                                                <h3 class="item-title">
+                                                    <?php echo $cantidadProductos['nombreproducto'] ?></h3>
+                                                <img class="item-image " src="./img/plato.png">
+                                                <h4 class="item-price text-center">$
+                                                    <?php echo $cantidadProductos['precio'] ?>
+                                                </h4>
+                                                <div class="display-flex">
+                                                    <button type="button" class="btn btn-warning btn-sm mr-2"
+                                                        name="editar"
+                                                        onclick="location.href='editarPlato.php?nombreproducto=<?php echo $cantidadProductos['nombreproducto'] ?>&precio=<?php echo $cantidadProductos['precio'] ?>&nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>'">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                            fill="currentColor" class="bi bi-pencil-square"
+                                                            viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                            <path fill-rule="evenodd"
+                                                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div><!-- /.col-lg-4 -->
+                                        <?php
                             }
                             ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <!-- Termina seccion de platos -->
+                    <div class="col col-md-5">
+                        <h1>boton añadir</h1>
+                    </div>
                 </div>
             </div>
+
             <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
         </body>
     </main>
