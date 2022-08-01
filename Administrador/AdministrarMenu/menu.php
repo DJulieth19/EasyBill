e79f8c3c9b8ff594bef475589cea3ba6881464b3
Derly Varón <djvd2001@gmail.com>
2022-08-01 18:21:04 -0500

    cambiando imagen

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -130,7 +130,7 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                         <div class="col-4 functions" data-bs-toggle="modal" data-bs-target="#añadir">
                             <button type="button" class="btn btn-info rounded-circle">
                                 <img class="item-image " src="./img/plato2.png">
-                                <h5>Añadir plato</h5>
+                                <h7>Añadir plato</h7>
                             </button>
                         </div>
                         <!-- Modals -->
