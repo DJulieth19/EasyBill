f5639a03518791c3c36ec211414b1b235fd79318
Derly Varón <djvd2001@gmail.com>
2022-08-01 19:17:41 -0500

    Update menu.php

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -91,12 +91,12 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                                                 <?php echo $cantidadProductos['nombreproducto'] ?></h3>
                                             <img class="item-image " src="./img/plato.png">
                                             <div class="row justify-content-end">
-                                                <div class="col">
+                                                <div class="col-auto">
                                                     <h5 class="item-price text-center">$
                                                         <?php echo $cantidadProductos['precio'] ?>
                                                     </h5>
                                                 </div>
-                                                <div class="col">
+                                                <div class="col-auto">
                                                     <button type="button" class="btn btn-warning btn-sm mr-2"
                                                         name="editar"
                                                         onclick="location.href='editarPlato.php?nombreproducto=<?php echo $cantidadProductos['nombreproducto'] ?>&precio=<?php echo $cantidadProductos['precio'] ?>&nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>'">
@@ -128,7 +128,7 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                 <div class="col col-3">
                     <div class="row">
                         <div class="col-4 functions" data-bs-toggle="modal" data-bs-target="#añadir">
-                            <button type="button" class="btn btn-info rounded-circle justify-content-end">
+                            <button type="button" class="btn btn-info pb-5 rounded-circle justify-content-end">
                                 <img src="./img/plato2.png">
                                 <h5 class="text-white">Añadir plato</h5>
                             </button>
