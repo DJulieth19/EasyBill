fa26c3749ac98401859ec946adb8a1818b01d127
Derly Varón <djvd2001@gmail.com>
2022-08-06 14:19:36 -0500

    Update menu.php

--- Administrador/AdministrarMenu/menu.php
+++ Administrador/AdministrarMenu/menu.php
@@ -177,7 +177,7 @@ $consultaPlatos = pg_query($conn, $queryPlatos);
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col-4">
                             <table class="table table-hover table-striped table-responsive p-4">
                                 <h5 class="mt-4 text-center">Acceso Rápido</h5>
                                 <tbody>
