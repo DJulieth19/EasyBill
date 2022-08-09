fe2c72d0559d68a04b9820ffc045685b828f03c1
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-08-09 10:51:12 -0500

    Update usuarios.php

--- Administrador/AdministrarUsuarios/usuarios.php
+++ Administrador/AdministrarUsuarios/usuarios.php
@@ -160,25 +160,26 @@ $consultaUsuarios = pg_query($conn, $queryUsuarios);
                                                     name="contraseña" placeholder="contraseña" required maxlength="10"
                                                     minlength="4">
                                                 <label>Contraseña </label>
-                                        <label for="username" class="form-label">Tipo de usuario</label>
-                                            <select type="text" class="form-control" value="Empleado"  name="tipo_usuario" placeholder="" required>
-                                                <option value="Empleado">Empleado</option>
-                                            </select>	
-                                        </div>
-                                        <div class="col-md-8">
-                                            <label for="pregunta" class="form-label">¿Cuál es la ciudad de nacimiento?</label>
-                                            <input type="number" class="form-control" name="ciudad" placeholder="" required>						
-                                        </div>
-                                        <div class="col-md-8">
-                                            <label for="pregunta" class="form-label">Nombre de un familiar</label>
-                                            <input type="date" class="form-control" name="nombreFamiliar" value="2022-01-01" placeholder="" required>	
-                                        </div>
-                                        <div class="col-md-8">
-                                            <label for="pregunta" class="form-label">Comida favorita</label>
-                                            <input type="number" class="form-control" name="comida" placeholder="" required>	
-                                        </div>
                                             </div>
-
+                                            <div class="form-floating mb-3">
+                                                 <label for="username" class="form-label">Tipo de usuario</label>
+                                                    <select type="text" class="form-control" value="Empleado"  name="tipo_usuario" placeholder="" required>
+                                                        <option value="Empleado">Empleado</option>
+                                                    </select>	
+                                                </div>
+                                                <div class="mb-1">
+                                                    <label for="pregunta" class="form-label">¿Cuál es la ciudad de nacimiento?</label>
+                                                    <input type="text" class="form-control" name="ciudad" placeholder="" required>						
+                                                </div>
+                                                <div class="mb-1">
+                                                    <label for="pregunta" class="form-label">Nombre de un familiar</label>
+                                                    <input type="text" class="form-control" name="nombreFamiliar" value="2022-01-01" placeholder="" required>	
+                                                </div>
+                                                <div class="mb-1">
+                                                    <label for="pregunta" class="form-label">Comida favorita</label>
+                                                    <input type="text" class="form-control" name="comida" placeholder="" required>	
+                                                </div>
+                                            </div>
                                             <div class="form-group">
                                                 <input type="file" id="archivo" name="archivo" class="form-control-file"
                                                     accept="image/*">
