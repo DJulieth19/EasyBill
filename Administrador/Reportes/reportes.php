a3201c47bac68d0f3da6b5bdc2b9ad820c6155f9
Derly Varón <djvd2001@gmail.com>
2022-08-11 22:15:00 -0500

    nav bar

--- Administrador/Reportes/reportes.php
+++ Administrador/Reportes/reportes.php
@@ -33,7 +33,7 @@ $consultaEmpleado = pg_query($conn, $queryEmpleado);
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
                 <h3>&nbsp &nbsp</h3>
-                <h3>Reportes Administrativos</h3>
+                <h3 class="text-white">Reportes Administrativos</h3>
                 <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
