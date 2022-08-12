c3e5e311d220763145b7a6690a09baf02569a3fb
Derly Varón <djvd2001@gmail.com>
2022-08-12 15:41:16 -0500

    alertas

--- Administrador/AdministrarMenu/editarPlato.php
+++ Administrador/AdministrarMenu/editarPlato.php
@@ -64,7 +64,7 @@ $codproducto=$usuario1['codproducto'];
 								<button type="button" onclick="location.href='./menu.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>'" class="btn btn-secondary">Volver</button>
 							</div>
 							<div class="col d-grid">
-								<button type="submit" class="btn btn-warning" onsubmit="Swal.fire({
+								<button type="submit" class="btn btn-warning" onSubmit="Swal.fire({
 									icon: 'success',
 									title: 'Editado correctamente',
 									showConfirmButton: false,
