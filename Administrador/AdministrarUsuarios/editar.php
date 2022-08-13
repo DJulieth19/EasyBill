381ee775c2e6d8630aa35c77e27acd78a34bd293
Daniel Martinez <danielxz331@gmail.com>
2022-08-13 17:46:22 -0500

    Update editar.php

--- Administrador/AdministrarUsuarios/editar.php
+++ Administrador/AdministrarUsuarios/editar.php
@@ -12,7 +12,7 @@ $tipo_usuario=$_POST['tipo_usuario'];
 $cedula=$_POST['cedula'];
 $fexpedicion = $_POST['fexpedicion'];
 $celular = $_POST['celular'];
-$queryUsuarios = "UPDATE usuarios SET nombre_usuario='$nombre_usuario',contraseña='$contraseña',tipo_usuario='$tipo_usuario' WHERE id_usuario='$id_usuario'";
+$queryUsuarios = "UPDATE usuarios SET nombre_usuario='$nombre_usuario',contraseña='$contraseña',tipo_usuario='$tipo_usuario'";
 $editarUsuarios = pg_query($conn, $queryUsuarios);
 $queryRecuperacion = "UPDATE recuperacion SET cedula='$cedula',fexpedicion='$fexpedicion',celular='$celular' WHERE id_usuario='$id_usuario'";
 $editarRecuperacion= pg_query($conn, $queryRecuperacion);
