7a27199a28223f9334cfd09686addd7cea2790a3
David Acosta <dacostaojeda2000@gmail.com>
2022-08-13 16:45:14 -0500

    Update index.php

--- Empleado/index.php
+++ Empleado/index.php
@@ -161,7 +161,7 @@
         <h3 class="fw-normal">Historial de pedidos</h3>
       </div><!-- /.col-lg-4 -->
       <div class="col-lg-4">
-        <a href="https://example.com/">
+        <a href="./cerrarcaja.php">
           <img src="../../Imagenes/arqueo.png"  width="160" height="160" role="img" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em"></text></svg>
         </a> 
         <h3 class="fw-normal">Finalizar jornada</h3>
