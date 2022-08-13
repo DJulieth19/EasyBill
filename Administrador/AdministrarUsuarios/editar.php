6f940611d4719ac739f2356a367847f13fda295d
Daniel Martinez <danielxz331@gmail.com>
2022-08-13 17:51:21 -0500

    Update editar.php

--- Administrador/AdministrarUsuarios/editar.php
+++ Administrador/AdministrarUsuarios/editar.php
@@ -5,14 +5,20 @@
 <?php 
 require_once("../../Database.php");
 $conn = conectardb();
-$id_usuario=$_GET['id_usuario'];
+
 $nombre_usuario=$_POST['nombre_usuario'];
 $contraseña=$_POST['contraseña'];
 $tipo_usuario=$_POST['tipo_usuario'];
 $cedula=$_POST['cedula'];
 $fexpedicion = $_POST['fexpedicion'];
 $celular = $_POST['celular'];
-$queryUsuarios = "UPDATE usuarios SET nombre_usuario='$nombre_usuario',contraseña='$contraseña',tipo_usuario='$tipo_usuario'";
+
+$queryUsuariosC = "SELECT * from usuarios WHERE nombre_usuario = '$nombre_usuario' AND contraseña = '$contraseña'";
+$consultaUsuarios = pg_query($conn, $queryUsuariosC);
+$tipo= pg_fetch_array($consultaUsuarios);
+$id_usuario=$tipo['id_usuario'];
+
+$queryUsuarios = "UPDATE usuarios SET nombre_usuario='$nombre_usuario',contraseña='$contraseña',tipo_usuario='$tipo_usuario' WHERE id_usuario='$id_usuario'";
 $editarUsuarios = pg_query($conn, $queryUsuarios);
 $queryRecuperacion = "UPDATE recuperacion SET cedula='$cedula',fexpedicion='$fexpedicion',celular='$celular' WHERE id_usuario='$id_usuario'";
 $editarRecuperacion= pg_query($conn, $queryRecuperacion);
