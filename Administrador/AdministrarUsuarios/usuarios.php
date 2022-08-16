1877f8f4840c06df8d5c5124a7e793864836b47c
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-08-16 09:30:05 -0500

    Update usuarios.php

--- Administrador/AdministrarUsuarios/usuarios.php
+++ Administrador/AdministrarUsuarios/usuarios.php
@@ -46,9 +46,9 @@ $consultaUsuarios = pg_query($conn, $queryUsuarios);
                             </div>
                             <a class="btn btn-custom "
                                 href="../index.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>">Volver</a>
-                            <a type="button" class="nav-bar-icons" href="https://sites.google.com/unillanos.edu.co/wikibill/inicio/administrador/administrar-usuarios">
+                            <a type="button" class="nav-bar-icons">
                                 <div class="dropdown">
-                                    <a class="btn btn-dark" href="#" role="button">
+                                    <a class="btn btn-dark" href="#" role="button" href="https://sites.google.com/unillanos.edu.co/wikibill/inicio/administrador/administrar-usuarios">
                                         <svg xmlns="http://www.w3.org/2000/svg" color="white" width="25" height="25"
                                             fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                                             <path
