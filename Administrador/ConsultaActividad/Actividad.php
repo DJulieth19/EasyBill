99fb3708afe45c9ab3d8466fa0e46d491b4a68e5
DJulieth19 <88950595+DJulieth19@users.noreply.github.com>
2022-08-16 09:36:26 -0500

    Update Actividad.php

--- Administrador/ConsultaActividad/Actividad.php
+++ Administrador/ConsultaActividad/Actividad.php
@@ -53,7 +53,7 @@
                                 href="../index.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>">Volver</a>
                             <a type="button" class="nav-bar-icons">
                                 <div class="dropdown">
-                                    <a class="btn btn-dark" href="#" role="button">
+                                    <a class="btn btn-dark" href="https://sites.google.com/unillanos.edu.co/wikibill/inicio/administrador/consulta-de-actividad" role="button">
                                         <svg xmlns="http://www.w3.org/2000/svg" color="white" width="25" height="25"
                                             fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
                                             <path
