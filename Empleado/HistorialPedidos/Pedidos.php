43f23e3ff36b29ae3217011237e7e5eb5ab5e44c
Derly Varón <djvd2001@gmail.com>
2022-08-18 02:35:35 -0500

    Update Pedidos.php

--- Empleado/HistorialPedidos/Pedidos.php
+++ Empleado/HistorialPedidos/Pedidos.php
@@ -46,25 +46,29 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <div class="nav-bar">
                         <form class="d-flex" role="search">
-                            <div class="NombreUser">
+                            <div class="NombreUser bg-dark">
                                 <thead>
                                     <th>
                                     <td><?php echo $tipo ?> &nbsp &nbsp <br>&nbsp &nbsp <?php echo $usuario ?> </td>
                                     </th>
                                 </thead>
                             </div>
-                            <a class="btn btn-custom "
-                                href="../index.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>&id_usuario=<?php echo $idU?>">Volver</a>
-                            <a type="button" class="nav-bar-icons">
-                                <div class="dropdown">
-                                    <a class="btn btn-dark" href="#" role="button">
-                                        <svg xmlns="http://www.w3.org/2000/svg" color="white" width="25" height="25"
-                                            fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
-                                            <path
-                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
-                                        </svg>
-                                    </a>
-                            </a>
+                            <div class="bg-dark">
+
+                                <a class="btn btn-custom bg-white"
+                                    href="../index.php?nombre=<?php echo $usuario?>&tipoUsuario=<?php echo $tipo?>&id_usuario=<?php echo $idU?>">Volver</a>
+                                <a type="button" class="nav-bar-icons">
+                            </div>
+
+                            <div class="dropdown bg-dark">
+                                <a class="btn " href="#" role="button">
+                                    <svg xmlns="http://www.w3.org/2000/svg" color="white" width="25" height="25"
+                                        fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
+                                    </svg>
+                                </a>
+                                </a>
                         </form>
                     </div>
                 </div>
@@ -144,7 +148,7 @@
                                             <?php
                               while($row = pg_fetch_array($consulta)){
                             ?>
-                                            <tr>                                              
+                                            <tr>
                                                 <td class="col-2 text-center"><?php echo $row['nombrecliente'] ?></td>
                                                 <td class="col-3 text-center"><?php echo $row['total'] ?></td>
                                                 <td class="col-3 text-center"><?php echo $row['fecha'] ?></td>
@@ -178,7 +182,7 @@
                             $suma= pg_fetch_array($consultaTotal);
                             $Total = $suma['sum']
             ?>
-            
+
 
                             <script src="../../assets/dist/js/bootstrap.bundle.min.js"></script>
     </body>
